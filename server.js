05b9a21deb6f9e168e92ff1f9ba95d70503a3c4b
Pinto18 <pinot18.nd@gmail.com>
2017-10-13 13:37:05 -0400

    Finished UI

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var express = require('express');
-var app = express();
-app.set('views', './views');
-app.set('view engine', 'jade');
+var http = require('http');  //importing http module
+var fs = require('fs');  //importing File System module
 
-app.listen(9000);    //setting the port to run the application on
+const PORT = 3000;   //setting port number for the web app; will run on http://localhost:3000
+fs.readFile( './views/home.html',   //giving fs path to the home page file
+	     function(error, html)  //giving fs function to perform on home.html
+             {
+             	if(error) throw error;
+
+                http.createServer( function(request, response)
+                                   {
+				      response.writeHeader( 200,
+							    { "Content-Type" : "text/html" }
+							  );
+				      response.write(html);   //writing html page to server
+                                      response.end();
+                                   }
+                                 ).listen(PORT);   //telling server what port to connect to
+             }
+           );
