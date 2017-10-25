e5d1729f77a2fcfe522a3558c2c47458a01aa76f
dilipMaharjan <dilipmaharjann@gmail.com>
2017-10-25 15:15:50 -0400

    server working

---
+++ api-server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to mongoose
+
+mongoose.connect('mongodb://localhost/bible_api');
+var db = mongoose.connection;
+
+//using body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(8001);
+
+//rest
+app.get('/', function (req, res) {
+    res.send('Welcome...');
+});
+app.post("/get_verse", (req, res) => {
+    let book = req.body.book;
+    let chapter = req.body.chapter;
+    let verse = req.body.verse
+    var filter = {};
+    if (book && chapter && verse) {
+        filter = { book: book, chapter: chapter, verse: verse };
+        db.collection('bible').find(filter, { _id: 0, book: 1, chapter: 1, verse: 1, title: 1 }).toArray((error, response) => {
+            if (error) {
+                res.send({ "error": "Verse not found." });
+            } else {
+                res.send(response);
+            }
+        });
+    } else {
+        res.send({ "error": "Please provide necessary data." })
+    }
+
+});
--- server.js
+++ server.js
@@ -2,19 +2,18 @@ var http = require('http');  //importing http module
 var fs = require('fs');  //importing File System module
 
 const PORT = 3000;   //setting port number for the web app; will run on http://localhost:3000
-fs.readFile( './views/home.html',   //giving fs path to the home page file
-	     function(error, html)  //giving fs function to perform on home.html
-             {
-             	if(error) throw error;
+fs.readFile('./views/home.html',   //giving fs path to the home page file
+  function (error, html)  //giving fs function to perform on home.html
+  {
+    if (error) throw error;
 
-                http.createServer( function(request, response)
-                                   {
-				      response.writeHeader( 200,
-							    { "Content-Type" : "text/html" }
-							  );
-				      response.write(html);   //writing html page to server
-                                      response.end();
-                                   }
-                                 ).listen(PORT);   //telling server what port to connect to
-             }
-           );
+    http.createServer(function (request, response) {
+      response.writeHeader(200,
+        { "Content-Type": "text/html" }
+      );
+      response.write(html);   //writing html page to server
+      response.end();
+    }
+    ).listen(PORT);   //telling server what port to connect to
+  }
+);
